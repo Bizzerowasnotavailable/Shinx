9531a20656d96f079aa5b2b01ae3454e1eec393a
bizzero <bizzero@fisso>
2026-03-22 11:37:18 +0100

    added nested folders in mkdir using -p

--- Shinx/utils/core_mkdir.cs
+++ Shinx/utils/core_mkdir.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// TODO: refactor
+
 namespace Shinx.utils
 {
     public class core_mkdir : ICommand
@@ -19,18 +21,52 @@ namespace Shinx.utils
                 return;
             }
 
-            args[0] = args[0].Replace(' ', '_');
-            string path = args[0].StartsWith(@"0:\") ? args[0] : Shell.currentDirectory + args[0];
+            // creating -p argument (might be broken)
+            // update: it wasn't broken lmao
+            bool createParents = false;
+            string dirArg = null;
+
+            foreach (var arg in args)
+            {
+                if (arg == "-p")
+                    createParents = true;
+                else
+                    dirArg = arg;
+            }
+
+            if (dirArg == null)
+            {
+                Console.WriteLine("usage: mkdir [-p] <directory>");
+                return;
+            }
+
+            dirArg = dirArg.Replace(' ', '_');
+            string fullPath = dirArg.StartsWith(@"0:\") ? dirArg : Shell.currentDirectory + dirArg;
+
+            fullPath = fullPath.Replace('/', '\\');
 
             try
             {
-                if (Directory.Exists(path))
+                if (Directory.Exists(fullPath))
                 {
-                    Console.WriteLine("mkdir: directory already exists: " + args[0]);
+                    Console.WriteLine("mkdir: directory already exists: " + dirArg);
                     return;
                 }
-
-                Directory.CreateDirectory(path);
+                if (createParents)
+                {
+                    CreateRecursive(fullPath);
+                }
+                else
+                {
+                    string parent = Path.GetDirectoryName(fullPath);
+                    if (!string.IsNullOrEmpty(parent) && !Directory.Exists(parent))
+                    {
+                        Console.WriteLine("mkdir: parent directory does not exist: " + parent);
+                        Console.WriteLine("hint: add -p to create parent directories");
+                        return;
+                    }
+                    Directory.CreateDirectory(fullPath);
+                }
             }
             catch (Exception e)
             {
@@ -38,5 +74,26 @@ namespace Shinx.utils
             }
         }
 
+        private void CreateRecursive(string fullPath)
+        {
+            var parts = new List<string>();
+            string current = fullPath;
+            while (!string.IsNullOrEmpty(current))
+            {
+                parts.Insert(0, current);
+                string parent = Path.GetDirectoryName(current);
+
+                if (parent == current || string.IsNullOrEmpty(parent))
+                    break;
+                current = parent;
+            }
+            foreach (var segment in parts)
+            {
+                if (!Directory.Exists(segment))
+                {
+                    Directory.CreateDirectory(segment);
+                }
+            }
+        }
     }
 }
