522f10a14e1f8368c93eaa4a8233347415380b90
Cristian Izzo <izzocristian03@gmail.com>
2026-03-24 17:15:02 +0100

    (coreutils) edit 3.0)

--- Shinx/utils/core_edit.cs
+++ Shinx/utils/core_edit.cs
@@ -2,14 +2,20 @@
 using System.IO;
 using System.Collections.Generic;
 
-
 namespace Shinx.Commands
 {
     public class core_edit : ICommand
     {
         private List<string> lines = new List<string>();
         private int currentLine = 0;
+        private int currentCol = 0;
+        private int topLine = 0;
+        private int leftChar = 0;
+
         private string path = "";
+        private bool running = true;
+        private string statusMessage = "";
+        private bool _needsFullRedraw = true;
 
         public void Execute(string[] args, HashSet<char> parameters)
         {
@@ -21,209 +27,212 @@ namespace Shinx.Commands
 
             path = args[0].StartsWith(@"0:\") ? args[0] : Shell.currentDirectory + args[0];
 
-            if (File.Exists(path) && !PermissionManager.CanAccess(path, UserManager.currentUser))
-            {
-                Console.WriteLine($"edit: permission denied: {args[0]}");
-                return;
-            }
-
-            lines = new List<string>();
-            currentLine = 0;
-
+            lines.Clear();
             if (File.Exists(path))
             {
                 lines.AddRange(File.ReadAllLines(path));
-                Console.WriteLine($"loaded {args[0]} ({lines.Count} lines)");
+            }
+            if (lines.Count == 0) lines.Add("");
+
+            currentLine = 0;
+            currentCol = 0;
+            topLine = 0;
+            leftChar = 0;
+            running = true;
+            _needsFullRedraw = true;
+            statusMessage = $"Editing {Path.GetFileName(path)}";
+
+            while (running)
+            {
+                Render();
+                HandleInput();
+            }
+
+            Console.Clear();
+        }
+
+        private void Render()
+        {
+            int screenWidth = Console.WindowWidth;
+            int screenHeight = Console.WindowHeight;
+            int viewHeight = screenHeight - 4;
+            int viewWidth = screenWidth - 4;
+
+            int oldTop = topLine;
+            int oldLeft = leftChar;
+
+            if (currentLine < topLine) topLine = currentLine;
+            if (currentLine >= topLine + viewHeight) topLine = currentLine - viewHeight + 1;
+            if (currentCol < leftChar) leftChar = currentCol;
+            if (currentCol >= leftChar + viewWidth) leftChar = currentCol - viewWidth + 1;
+
+            if (oldTop != topLine || oldLeft != leftChar) _needsFullRedraw = true;
+
+            if (_needsFullRedraw)
+            {
+                Console.Clear();
+                Console.SetCursorPosition(0, 0);
+                Console.BackgroundColor = ConsoleColor.Gray;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.Write($" SHINX EDIT | L: {currentLine + 1} C: {currentCol + 1} | {path}".PadRight(screenWidth - 1));
+                Console.ResetColor();
+
+                for (int i = 0; i < viewHeight; i++)
+                {
+                    int lineIndex = topLine + i;
+                    Console.SetCursorPosition(0, i + 1);
+                    DrawLine(lineIndex, viewWidth);
+                }
+
+                Console.SetCursorPosition(0, screenHeight - 2);
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write(statusMessage.PadRight(screenWidth - 1));
+                Console.SetCursorPosition(0, screenHeight - 1);
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.Write(" ^O Save    ^X Exit    ^L GoTo Line ".PadRight(screenWidth - 1));
+                Console.ResetColor();
+
+                _needsFullRedraw = false;
             }
             else
             {
-                Console.WriteLine($"new file: {args[0]}");
+                Console.SetCursorPosition(0, 0);
+                Console.BackgroundColor = ConsoleColor.Gray;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.Write($" SHINX EDIT | L: {currentLine + 1} C: {currentCol + 1} | {path}".PadRight(screenWidth - 1));
+                Console.ResetColor();
+
+                Console.SetCursorPosition(0, (currentLine - topLine) + 1);
+                DrawLine(currentLine, viewWidth);
             }
 
-            PrintFile();
-            Console.WriteLine("type :h for help");
+            int cursorX = (currentCol - leftChar) + 2;
+            int cursorY = (currentLine - topLine) + 1;
+            Console.SetCursorPosition(Math.Clamp(cursorX, 0, screenWidth - 1), Math.Clamp(cursorY, 0, screenHeight - 1));
+        }
 
+        private void DrawLine(int lineIndex, int viewWidth)
+        {
+            if (lineIndex < lines.Count)
+            {
+                string fullLine = lines[lineIndex];
+                string visibleText = (fullLine.Length > leftChar)
+                    ? fullLine.Substring(leftChar, Math.Min(fullLine.Length - leftChar, viewWidth))
+                    : "";
+
+                if (lineIndex == currentLine)
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkBlue;
+                    Console.Write("> " + visibleText.PadRight(viewWidth));
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.Write("  " + visibleText.PadRight(viewWidth));
+                }
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.Write("~".PadRight(viewWidth + 2));
+                Console.ResetColor();
+            }
+        }
+
+        private void HandleInput()
+        {
+            ConsoleKeyInfo key = Console.ReadKey(true);
+
+            if ((key.Modifiers & ConsoleModifiers.Control) != 0)
+            {
+                switch (key.Key)
+                {
+                    case ConsoleKey.O: SaveFile(); _needsFullRedraw = true; break;
+                    case ConsoleKey.X: running = false; break;
+                    case ConsoleKey.L: ShowGoToLinePrompt(); _needsFullRedraw = true; break;
+                }
+                return;
+            }
+
+            string lineText = lines[currentLine];
+            switch (key.Key)
+            {
+                case ConsoleKey.UpArrow:
+                case ConsoleKey.DownArrow:
+                    if (key.Key == ConsoleKey.UpArrow && currentLine > 0) currentLine--;
+                    else if (key.Key == ConsoleKey.DownArrow && currentLine < lines.Count - 1) currentLine++;
+                    if (currentCol > lines[currentLine].Length) currentCol = lines[currentLine].Length;
+                    _needsFullRedraw = true;
+                    break;
+
+                case ConsoleKey.LeftArrow:
+                    if (currentCol > 0) currentCol--;
+                    break;
+
+                case ConsoleKey.RightArrow:
+                    if (currentCol < lineText.Length) currentCol++;
+                    break;
+
+                case ConsoleKey.Enter:
+                    string rem = lineText.Substring(currentCol);
+                    lines[currentLine] = lineText.Substring(0, currentCol);
+                    lines.Insert(currentLine + 1, rem);
+                    currentLine++;
+                    currentCol = 0;
+                    _needsFullRedraw = true;
+                    break;
+
+                case ConsoleKey.Backspace:
+                    if (currentCol > 0)
+                    {
+                        lines[currentLine] = lineText.Remove(currentCol - 1, 1);
+                        currentCol--;
+                    }
+                    else if (currentLine > 0)
+                    {
+                        int prevLen = lines[currentLine - 1].Length;
+                        lines[currentLine - 1] += lines[currentLine];
+                        lines.RemoveAt(currentLine);
+                        currentLine--;
+                        currentCol = prevLen;
+                        _needsFullRedraw = true;
+                    }
+                    break;
+
+                default:
+                    if (!char.IsControl(key.KeyChar))
+                    {
+                        lines[currentLine] = lineText.Insert(currentCol, key.KeyChar.ToString());
+                        currentCol++;
+                    }
+                    break;
+            }
+        }
+
+        private void SaveFile()
+        {
             try
             {
-                while (true)
-                {
-                    Console.Write($":{currentLine + 1}> ");
-                    string input = ReadLine();
-
-                    if (input.StartsWith(":d "))
-                    {
-                        try
-                        {
-                            int lineNum = int.Parse(input.Substring(3));
-                            if (lineNum >= 1 && lineNum <= lines.Count)
-                            {
-                                lines.RemoveAt(lineNum - 1);
-                                if (currentLine >= lines.Count)
-                                    currentLine = lines.Count;
-                                PrintFile();
-                            }
-                            else
-                            {
-                                Console.WriteLine("edit: line out of range");
-                            }
-                        }
-                        catch
-                        {
-                            Console.WriteLine("edit: invalid line number");
-                        }
-                        continue;
-                    }
-
-                    if (input.StartsWith(":g "))
-                    {
-                        if (int.TryParse(input.Substring(3), out int lineNum))
-                        {
-                            if (lineNum >= 1 && lineNum <= lines.Count)
-                            {
-                                currentLine = lineNum - 1;
-                                Console.WriteLine($"jumped to line {lineNum}: {lines[currentLine]}");
-                            }
-                            else
-                            {
-                                Console.WriteLine("edit: line out of range");
-                            }
-                        }
-                        continue;
-                    }
-
-                    switch (input)
-                    {
-                        case ":q":
-                            return;
-                        case ":w":
-                            File.WriteAllLines(path, lines.ToArray());
-                            PermissionManager.SetDefault(path, UserManager.currentUser);
-                            Console.WriteLine($"saved {args[0]}");
-                            break;
-                        case ":wq":
-                            File.WriteAllLines(path, lines.ToArray());
-                            PermissionManager.SetDefault(path, UserManager.currentUser);
-                            Console.WriteLine($"saved {args[0]}");
-                            return;
-                        case ":p":
-                            PrintFile();
-                            break;
-                        case ":n":
-                            currentLine = lines.Count;
-                            Console.WriteLine($"moved to new line {currentLine + 1}");
-                            break;
-                        case ":h":
-                            Console.WriteLine(":w - save");
-                            Console.WriteLine(":wq - save and quit");
-                            Console.WriteLine(":q - quit");
-                            Console.WriteLine(":p - print file");
-                            Console.WriteLine(":g <line> - go to line");
-                            Console.WriteLine(":d <line> - delete line");
-                            Console.WriteLine(":n - go to new line at end");
-                            break;
-                        default:
-                            if (currentLine >= lines.Count)
-                                lines.Add(input);
-                            else
-                                lines[currentLine] = input;
-                            currentLine++;
-                            PrintFile();
-                            break;
-                    }
-                }
+                File.WriteAllLines(path, lines.ToArray());
+                statusMessage = "FILE SAVED";
             }
             catch (Exception e)
             {
-                Console.WriteLine($"edit: {e.Message}");
+                statusMessage = "SAVE ERROR: " + e.Message;
             }
         }
 
-        private void PrintFile()
+        private void ShowGoToLinePrompt()
         {
-            Console.WriteLine($"--- {path} ---");
-            if (lines.Count == 0)
+            statusMessage = "Line: ";
+            Render();
+            string input = Console.ReadLine();
+            if (int.TryParse(input, out int l) && l > 0 && l <= lines.Count)
             {
-                Console.WriteLine("(empty)");
+                currentLine = l - 1;
+                currentCol = 0;
             }
-            else
-            {
-                for (int i = 0; i < lines.Count; i++)
-                {
-                    string marker = i == currentLine ? ">" : " ";
-                    Console.WriteLine($"{marker}{i + 1}: {lines[i]}");
-                }
-            }
-            Console.WriteLine("---");
-        }
-
-        private string ReadLine()
-        {
-            string input = "";
-            int cursorPos = 0;
-
-            while (true)
-            {
-                ConsoleKeyInfo key = Console.ReadKey(true);
-
-                switch (key.Key)
-                {
-                    case ConsoleKey.Enter:
-                        Console.WriteLine();
-                        return input;
-
-                    case ConsoleKey.Backspace:
-                        if (cursorPos > 0)
-                        {
-                            input = input.Substring(0, cursorPos - 1) + input.Substring(cursorPos);
-                            cursorPos--;
-                            RedrawLine(input, cursorPos);
-                        }
-                        break;
-
-                    case ConsoleKey.Delete:
-                        if (cursorPos < input.Length)
-                        {
-                            input = input.Substring(0, cursorPos) + input.Substring(cursorPos + 1);
-                            RedrawLine(input, cursorPos);
-                        }
-                        break;
-
-                    case ConsoleKey.LeftArrow:
-                        if (cursorPos > 0)
-                        {
-                            cursorPos--;
-                            Console.CursorLeft--;
-                        }
-                        break;
-
-                    case ConsoleKey.RightArrow:
-                        if (cursorPos < input.Length)
-                        {
-                            cursorPos++;
-                            Console.CursorLeft++;
-                        }
-                        break;
-
-                    default:
-                        if (key.KeyChar != '\0')
-                        {
-                            input = input.Substring(0, cursorPos) + key.KeyChar + input.Substring(cursorPos);
-                            cursorPos++;
-                            RedrawLine(input, cursorPos);
-                        }
-                        break;
-                }
-            }
-        }
-
-        private void RedrawLine(string input, int cursorPos)
-        {
-            int promptLen = ($":{currentLine + 1}> ").Length;
-            Console.CursorLeft = promptLen;
-            Console.Write(new string(' ', Console.WindowWidth - promptLen - 1));
-            Console.CursorLeft = promptLen;
-            Console.Write(input);
-            Console.CursorLeft = promptLen + cursorPos;
         }
     }
 }
