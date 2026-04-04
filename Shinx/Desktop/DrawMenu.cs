b3746eb184f5bde9af22696cbb1aac7e66c3e2a5
Cristian Izzo <izzocristian03@gmail.com>
2026-04-04 19:57:36 +0200

    allow menu to scroll when app overflow

--- Shinx/Desktop/DrawMenu.cs
+++ Shinx/Desktop/DrawMenu.cs
@@ -8,38 +8,98 @@ namespace Shinx.GUI
     {
         public static bool menu = false;
 
+        private static int scrollOffset = 0;
+        private const int MaxVisible = 7;
+        private const int BtnH = 25;
+        private const int BtnGap = 30;
+        private const int TaskbarY = 570;
+
         public static void update(Point CurMouse, Canvas vbe)
         {
             if (!menu) return;
 
-            vbe.DrawFilledRectangle(Color.FromArgb(255, 0, 0, 150), 0, 240, 150, 330);
-            vbe.DrawRectangle(Color.White, 0, 240, 150, 330);
+            int totalApps = AppManager.Apps.Count;
+            int maxScroll = totalApps - MaxVisible;
+            if (maxScroll < 0) maxScroll = 0;
+            if (scrollOffset > maxScroll) scrollOffset = maxScroll;
+            if (scrollOffset < 0) scrollOffset = 0;
 
-            ASC16.DrawACSIIString(vbe, "Hello, " + UserManager.currentUser, Color.Yellow, (uint)10, (uint)250);
+            bool hasScroll = totalApps > MaxVisible;
+            int visibleCount = totalApps < MaxVisible ? totalApps : MaxVisible;
 
-            vbe.DrawLine(Color.White, 5, 268, 145, 268);
+            int menuH = 30 + 10 + (hasScroll ? BtnGap : 0) + visibleCount * BtnGap + (hasScroll ? BtnGap : 0) + 10 + BtnH + 5;
 
-            int yOffset = 275;
-            foreach (var app in AppManager.Apps)
+            int menuY = TaskbarY - menuH;
+
+            vbe.DrawFilledRectangle(Color.FromArgb(255, 0, 0, 150), 0, menuY, 150, menuH);
+            vbe.DrawRectangle(Color.White, 0, menuY, 150, menuH);
+
+            ASC16.DrawACSIIString(vbe, "Hello, " + UserManager.currentUser, Color.Yellow, 10, (uint)(menuY + 8));
+            vbe.DrawLine(Color.White, 5, menuY + 26, 145, menuY + 26);
+
+            int yOffset = menuY + 33;
+
+            if (hasScroll)
             {
-                vbe.DrawFilledRectangle(Color.Teal, 5, yOffset, 140, 25);
+                Color arrowColor = scrollOffset > 0 ? Color.White : Color.DarkGray;
+                vbe.DrawFilledRectangle(Color.FromArgb(255, 0, 0, 100), 5, yOffset, 140, BtnH);
+                ASC16.DrawACSIIString(vbe, "    ^ scroll up", arrowColor, 15, (uint)(yOffset + 5));
 
-                ASC16.DrawACSIIString(vbe, app.DisplayName, Color.White, (uint)15, (uint)(yOffset + 5));
+                if (Mouse.Click() && scrollOffset > 0 &&
+                    CurMouse.X > 5 && CurMouse.X < 145 &&
+                    CurMouse.Y > yOffset && CurMouse.Y < yOffset + BtnH)
+                {
+                    scrollOffset--;
+                }
 
-                if (Mouse.Click() && CurMouse.X > 5 && CurMouse.X < 145 && CurMouse.Y > yOffset && CurMouse.Y < yOffset + 25)
+                yOffset += BtnGap;
+            }
+
+            for (int i = scrollOffset; i < totalApps && i < scrollOffset + MaxVisible; i++)
+            {
+                var app = AppManager.Apps[i];
+
+                vbe.DrawFilledRectangle(Color.Teal, 5, yOffset, 140, BtnH);
+                string label = app.DisplayName;
+                if (label.Length > 14) label = label.Substring(0, 13) + ".";
+                ASC16.DrawACSIIString(vbe, label, Color.White, 15, (uint)(yOffset + 5));
+
+                if (Mouse.Click() &&
+                    CurMouse.X > 5 && CurMouse.X < 145 &&
+                    CurMouse.Y > yOffset && CurMouse.Y < yOffset + BtnH)
                 {
                     app.IsVisible = true;
                     WindowManager.windowToBringToFront = app.AppID;
                     menu = false;
+                    scrollOffset = 0;
                 }
-                yOffset += 30;
+
+                yOffset += BtnGap;
             }
 
-            vbe.DrawFilledRectangle(Color.DarkRed, 5, 540, 140, 25);
+            if (hasScroll)
+            {
+                Color arrowColor = scrollOffset < maxScroll ? Color.White : Color.DarkGray;
+                vbe.DrawFilledRectangle(Color.FromArgb(255, 0, 0, 100), 5, yOffset, 140, BtnH);
+                ASC16.DrawACSIIString(vbe, "    v scroll dn", arrowColor, 15, (uint)(yOffset + 5));
 
-            ASC16.DrawACSIIString(vbe, "Shutdown", Color.White, (uint)15, (uint)545);
+                if (Mouse.Click() && scrollOffset < maxScroll &&
+                    CurMouse.X > 5 && CurMouse.X < 145 &&
+                    CurMouse.Y > yOffset && CurMouse.Y < yOffset + BtnH)
+                {
+                    scrollOffset++;
+                }
 
-            if (Mouse.Click() && CurMouse.X > 5 && CurMouse.X < 145 && CurMouse.Y > 540 && CurMouse.Y < 565)
+                yOffset += BtnGap;
+            }
+
+            yOffset += 5;
+            vbe.DrawFilledRectangle(Color.DarkRed, 5, yOffset, 140, BtnH);
+            ASC16.DrawACSIIString(vbe, "Shutdown", Color.White, 15, (uint)(yOffset + 5));
+
+            if (Mouse.Click() &&
+                CurMouse.X > 5 && CurMouse.X < 145 &&
+                CurMouse.Y > yOffset && CurMouse.Y < yOffset + BtnH)
             {
                 Cosmos.System.Power.Shutdown();
             }
